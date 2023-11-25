d7b2c465170e5b41465d4ab6fb330c0e10acfc6c
SICADA <youssefkhaled3301@gmail.com>
2023-11-26 00:24:07 +0200

    Update en.js

--- src/assets/locales/en.js
+++ src/assets/locales/en.js
@@ -2,6 +2,9 @@ export const en = {
   translation: {
     common: {
       welcome: "Welcome!",
+      error: "Something went wrong, please try again later.",
+      messageSuccess: "Message sent successfully.",
+      backToHome: "home",
     },
     nav: {
       letsTalk: "Let's talk !",
@@ -11,7 +14,7 @@ export const en = {
       home: "Home",
     },
     hero: {
-      role: "Technical Consultant",
+      role: "Sr.TECHNICAL CONSULTANT",
       description:
         "Through meticulous analysis and innovative solutions, I guide businesses in navigating the complex landscape of technology.",
     },
@@ -50,6 +53,36 @@ export const en = {
       description:
         "I share insights, knowledge, and experiences on the dynamic world of software engineering. Dive into a realm where C#, clean code, and .NET form the cornerstone of discussions. Join me on this journey through the intricate landscapes of software development.",
       latestBlogs: "Latest blogs",
+    },
+    about: {
+      learnAbout: "Learn about me",
+      myCareer: "career",
+      myPersonal: "personal",
+      careerHeader: "myself and my career",
+      personalHeader: "my personality",
+      career: {
+        1: "Hello, I'm Mabrouk, a passionate Senior Technical Consultant, at eBiz Consulting GmbH, dedicated to shaping the future of software engineering and architecture. With a strong foundation in cutting-edge technologies and a relentless drive for innovation, I thrive on solving complex problems and building elegant, scalable solutions.",
+        2: "My journey in the world of software began in 2007. Since then, I have continuously honed my skills and expertise, staying at the forefront of emerging trends and paradigms in the field. My career has been a dynamic exploration of the ever-evolving landscape of software development.",
+        3: "I specialize in crafting robust, maintainable, and efficient software architectures. Whether it's designing microservices, embracing containerization and orchestration, or harnessing the power of cloud-native technologies, I'm committed to architectonic solutions that stand the test of time.",
+      },
+      personal: {
+        1: "What truly excites me is the opportunity to share knowledge and inspire others. Through speaking engagements, blog posts, and open-source contributions, I aim to contribute to the software engineering community and foster a culture of continuous learning and improvement.",
+        2: "I'm a big believer in the power of open-source software. I actively contribute to projects, including the .NET Foundation. Always on the lookout for new ways to give back, I believe in the collaborative spirit that fuels community-driven development. My commitment to advancing technology through collective effort is unwavering, and I'm eager for opportunities to make a meaningful impact in the open-source realm.",
+        3: "When I'm not coding, you can find me in the nature, cooking, or simply exploring the latest tech trends.",
+      },
+    },
+    contact: {
+      contactMe: "Contact me",
+      getInTouch: "Get in touch",
+      contactDescription: {
+        0: "Have a question?",
+        1: "I'm always open to new opportunities and collaborations. If you have a project in mind or just want to say hi, feel free to drop me a line.",
+        2: "Feel free to reach out to me via email or social media. I'll get back to you as soon as possible.",
+      },
+      name: "Name",
+      email: "Email",
+      message: "Message",
+      send: "Send",
     },
   },
 };
