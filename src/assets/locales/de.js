7962b316b4894f0fa8bd3ee393820a873f01fdbd
SICADA <youssefkhaled3301@gmail.com>
2023-12-18 22:29:11 +0200

    localization

--- src/assets/locales/de.js
+++ src/assets/locales/de.js
@@ -1,49 +1,89 @@
 export const de = {
   translation: {
     common: {
-      welcome: "Willkommen!",
+      welcome: "Welcome!",
+      error: "Something went wrong, please try again later.",
+      messageSuccess: "Message sent successfully.",
+      backToHome: "home",
     },
     nav: {
-      letsTalk: "Lass uns reden !",
+      letsTalk: "Let's talk !",
       blogs: "Blogs",
-      about: "Über",
-      contact: "Kontakt",
-      home: "Heim",
+      about: "About",
+      contact: "Contact",
+      home: "Home",
     },
     hero: {
-      role: "Sr. Technical Conultant",
+      role: "Sr. Technical Consultant",
       description:
-        "Durch sorgfältige Analysen und innovative Lösungen begleite ich Unternehmen dabei, sich in der komplexen Technologielandschaft zurechtzufinden.",
+        "Through meticulous analysis and innovative solutions, I guide businesses in navigating the complex landscape of technology.",
     },
     work: {
-      myWork: "Meine Arbeit",
-      discover: "Entdecke mehr",
-      myBook: "Über Null lachen",
+      myWork: "My work",
+      discover: "Discover more",
+      myBook: "Laughing at Null",
       bookHeader: {
-        1: "Über Null lachen, ein humorvoller Leitfaden dazu",
-        2: "Software-Leben.",
+        1: "Laughing at Null: A Humorous Guide To",
+        2: "Software Life.",
       },
       bookDescription:
-        "Entdecken Sie „Laughing at Null“ – eine einzigartige Reise in die fesselnde Welt der Softwareentwicklung, in der Code auf Komik und Technik auf Albernheit trifft.",
-      getYourCopy: "Holen Sie sich Ihr Exemplar",
+        'Discover "Laughing at Null" – a unique journey into the captivating world of software engineering, where code meets comedy and tech meets tomfoolery.',
+      getYourCopy: "Get your copy",
     },
     aboutSection: {
       header:
-        "Ein technischer Berater, der sich auf die Gestaltung der Zukunft des Software-Engineerings konzentriert",
+        "A technical consultant focused on shaping the future of software engineering",
       description:
-        "Ich bin auf die Entwicklung strategischer technologischer Rahmenbedingungen spezialisiert und konzentriere mich auf die Formulierung von Lösungen, die Ihren Geschäftszielen entsprechen. ",
-      aboutMe: "Über mich",
+        "Specialized in crafting strategic technological frameworks, my concentration lies in formulating solutions that resonate with your business goals. Prioritize your core objective of business expansion",
+      aboutMe: "About me",
     },
     blogsSection: {
-      publishedOn: "Veröffentlicht auf",
-      on: "An",
-      myBlogs: "Meine Blogs",
+      publishedOn: "Published on",
+      on: "on",
+      myBlogs: "My blogs",
       description:
-        "Ich schreibe über C#, sauberen Code, .NET, Softwareentwicklung und meine Reise als Entwickler.",
+        "I write about C#, clean code, .NET, software engineering, and my journey as a developer.",
     },
     footer: {
-      idea: "Hast du eine schöne Idee?",
-      hello: "Hallo",
+      idea: "have a nice idea? talk with me",
+      hello: "hello",
+    },
+    blogs: {
+      blogs: "Blogs",
+      description:
+        "I share insights, knowledge, and experiences on the dynamic world of software engineering. Dive into a realm where C#, clean code, and .NET form the cornerstone of discussions. Join me on this journey through the intricate landscapes of software development.",
+      latestBlogs: "Latest blogs",
+    },
+    about: {
+      learnAbout: "Learn about me",
+      myCareer: "career",
+      aboutMe: "about me",
+      myPersonal: "personal",
+      careerHeader: "myself and my career",
+      personalHeader: "my personality",
+      career: {
+        1: "Hello, I'm Mabrouk, a passionate Senior Technical Consultant, at eBiz Consulting GmbH, dedicated to shaping the future of software engineering and architecture. With a strong foundation in cutting-edge technologies and a relentless drive for innovation, I thrive on solving complex problems and building elegant, scalable solutions.",
+        2: "My journey in the world of software began in 2007. Since then, I have continuously honed my skills and expertise, staying at the forefront of emerging trends and paradigms in the field. My career has been a dynamic exploration of the ever-evolving landscape of software development.",
+        3: "I specialize in crafting robust, maintainable, and efficient software architectures. Whether it's designing microservices, embracing containerization and orchestration, or harnessing the power of cloud-native technologies, I'm committed to architectonic solutions that stand the test of time.",
+      },
+      personal: {
+        1: "What truly excites me is the opportunity to share knowledge and inspire others. Through speaking engagements, blog posts, and open-source contributions, I aim to contribute to the software engineering community and foster a culture of continuous learning and improvement.",
+        2: "I'm a big believer in the power of open-source software. I actively contribute to projects, including the .NET Foundation. Always on the lookout for new ways to give back, I believe in the collaborative spirit that fuels community-driven development. My commitment to advancing technology through collective effort is unwavering, and I'm eager for opportunities to make a meaningful impact in the open-source realm.",
+        3: "When I'm not coding, you can find me in the nature, cooking, or simply exploring the latest tech trends.",
+      },
+    },
+    contact: {
+      contactMe: "Contact me",
+      getInTouch: "Get in touch",
+      contactDescription: {
+        0: "Have a question?",
+        1: "I'm always open to new opportunities and collaborations. If you have a project in mind or just want to say hi, feel free to drop me a line.",
+        2: "Feel free to reach out to me via email or social media. I'll get back to you as soon as possible.",
+      },
+      name: "Name",
+      email: "Email",
+      message: "Message",
+      send: "Send",
     },
   },
 };
